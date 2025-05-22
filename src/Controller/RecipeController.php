2c80457c6a2f8d8ed43c8425fc747a50ada6df30
Neptune2k21 <116343729+Neptune2k21@users.noreply.github.com>
2025-05-22 11:18:05 +0200

    Partie 3 : CRUD des recettes - Créer une recette

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,7 +3,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Form\RecipeTypeForm;
 use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,6 +26,32 @@ final class RecipeController extends AbstractController
         
         return $this->render('pages/recipe/index.html.twig', [
             'recipes' => $recipes
+        ]);
+    }
+    
+    #[Route('/recipe/nouveau', name: 'recipe_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeTypeForm::class, $recipe);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recipe = $form->getData();
+            
+            $manager->persist($recipe);
+            $manager->flush();
+            
+            $this->addFlash(
+                'success',
+                'Votre recette a été créée avec succès !'
+            );
+            
+            return $this->redirectToRoute('app_recipe');
+        }
+        
+        return $this->render('pages/recipe/new.html.twig', [
+            'form' => $form
         ]);
     }
 }
---
+++ src/Form/RecipeTypeForm.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ingredient;
+use App\Entity\Recipe;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipeTypeForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '255'
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('time', IntegerType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'min' => 1,
+                    'max' => 1440
+                ],
+                'required' => false,
+                'label' => 'Temps (en minutes)',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('nbPersons', IntegerType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'min' => 1,
+                    'max' => 50
+                ],
+                'required' => false,
+                'label' => 'Nombre de personnes',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('diffculty', RangeType::class, [
+                'attr' => [
+                    'class' => 'form-range',
+                    'min' => 1,
+                    'max' => 5
+                ],
+                'required' => false,
+                'label' => 'Difficulté',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'min' => 1,
+                    'max' => 5
+                ],
+                'label' => 'Description',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'required' => false,
+                'label' => 'Prix',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('isFavorite', CheckboxType::class, [
+                'attr' => [
+                    'class' => 'form-check-input'
+                ],
+                'required' => false,
+                'label' => 'Favoris ?',
+                'label_attr' => [
+                    'class' => 'form-check-label'
+                ]
+            ])
+            ->add('ingredients', EntityType::class, [
+                'class' => Ingredient::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'label' => 'Les ingrédients',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ],
+                'label' => 'Créer ma recette'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipe::class,
+        ]);
+    }
+}
